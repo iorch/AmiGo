3385eb988e89284148cf55b6d0b4c7304110d2df
iorch <j.martinezortega@gmail.com>
2016-03-25 22:58:34 -0600

    adds testing of functionalities

--- test.cc
+++ test.cc
@@ -8,8 +8,12 @@ int main(int argc, char** argv) {
 
     std::cout << file_name << '\n';
     amigo::kifu k(file_name);
-    auto board = k.final_board();
+    auto board = k.position_at(12);
     board.draw();
+    for (auto& g : board.get_groups("white")){
+        board.draw_layer(g.first);
+        board.draw_layer(g.second);
+    }
 
     return 0;
 }
